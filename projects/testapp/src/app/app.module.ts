aede9a58dfd47e5b916d8ea432cfc041908671a7
Ignacio <ijpmorales@gmail.com>
2022-02-02 21:37:21 -0500

    (chore): Update Angular 13 - update web3modal-ts dependency (v1.3) - bump web3modal-angular version (v1.7)

--- projects/testapp/src/app/app.module.ts
+++ projects/testapp/src/app/app.module.ts
@@ -3,19 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Web3ModalModule, Web3ModalService, Web3ModalComponent } from '@mindsorg/web3modal-angular';
+import { Web3ModalModule, Web3ModalService } from '@mindsorg/web3modal-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    Web3ModalModule
-  ],
-  entryComponents: [Web3ModalComponent],
-  providers: [Web3ModalService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        Web3ModalModule
+    ],
+    providers: [Web3ModalService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
